c683305ec270b71156f5110f952b0d4ff6f2591a
Igor-NF <igor.fr@list.ru>
2023-12-11 15:56:27 +0300

    Добавил скобки для списка.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import modules.perimeter as perimeter
 
-p1 = perimeter.perimeter_func(2, 5, 4, 3)
+p1 = perimeter.perimeter_func([2, 5, 4, 3])
 print(p1)
 
-p2 = perimeter.perimeter_func(2, 5, 4, 3, 7, 6)
+p2 = perimeter.perimeter_func([2, 5, 4, 3, 7, 6])
 print(p2)
 
 print("Hello Git")
